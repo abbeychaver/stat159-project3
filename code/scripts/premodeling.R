d7e8cb55c227734908dca18b660027d48dadd3ba
= <=>
2016-11-21 14:40:20 -0800

    removing some rows where 0 should have been reported as NA

--- code/scripts/premodeling.R
+++ code/scripts/premodeling.R
@@ -47,16 +47,29 @@ write.csv(school_data, file = "data/gap_metrics_data.csv")
 cr_w_b <- school_data[, 1:6]
 cr_w_b <- na.omit(cr_w_b)
 cr_w_b$gap_completion_white_black = (cr_w_b$C150_4_WHITE-cr_w_b$C150_4_BLACK)/cr_w_b$C150_4_WHITE
+cr_w_b2 <- cr_w_b[cr_w_b$C150_4_WHITE != 0, ]
+cr_w_b2 <- cr_w_b2[cr_w_b2$C150_4_BLACK != 0, ]
+# Two historically black universities have 0% completion rates for students, remove these 
+cr_w_b <- cr_w_b2[is.finite(cr_w_b$gap_completion_white_black),]
 write.csv(cr_w_b, file="data/Completion_W_B.csv")
 
 cr_w_h <- school_data[, c(1:5,7)]
 cr_w_h <- na.omit(cr_w_h)
 cr_w_h$gap_completion_white_hispanic = (cr_w_h$C150_4_WHITE-cr_w_h$C150_4_HISP)/cr_w_h$C150_4_WHITE
-write.csv(cr_w_b, file="data/Completion_W_H.csv")
+cr_w_h1 <- cr_w_h[is.finite(cr_w_h$gap_completion_white_hispanic),]
+# Seems like some NA completion rates are reported as 0
+# Try removing these
+cr_w_h2 <- cr_w_h[cr_w_h$C150_4_WHITE != 0, ]
+cr_w_h2 <- cr_w_h2[cr_w_h2$C150_4_HISP != 0, ]
+cr_w_h2 <- cr_w_h2[is.finite(cr_w_h$gap_completion_white_hispanic),]
+write.csv(cr_w_h2, file="data/Completion_W_H.csv")
 
 cr_w_a <- school_data[, c(1:5, 8)]
 cr_w_a <- na.omit(cr_w_a)
 cr_w_a$gap_completion_white_asian = (cr_w_a$C150_4_WHITE-cr_w_a$C150_4_ASIAN)/cr_w_a$C150_4_WHITE
+cr_w_a2 <- cr_w_a[cr_w_a$C150_4_WHITE != 0, ]
+cr_w_a2 <- cr_w_a2[cr_w_a2$C150_4_ASIAN != 0, ]
+cr_w_a2 <- cr_w_a2[is.finite(cr_w_a$gap_completion_white_asian),]
 write.csv(cr_w_b, file="data/Completion_W_A.csv")
 
 # Separate incomes rates out to remove rows with NA values
