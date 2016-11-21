bf2b44b137bc3a0d24a2cf7c0f04f9eb1e2d20f5
= <=>
2016-11-21 14:08:44 -0800

    calculating separate gap metric dataframes for comparisons

--- code/scripts/premodeling.R
+++ code/scripts/premodeling.R
@@ -35,8 +35,35 @@ school_data$gap_completion_white_2mor = school_data$C150_4_WHITE-school_data$C15
 school_data$gap_completion_white_nra = school_data$C150_4_WHITE-school_data$C150_4_NRA
 school_data$gap_completion_white_unkn = school_data$C150_4_WHITE-school_data$C150_4_UNKN
 
+
 # Gap metrics of well-represented and underrepresented income groups
 school_data$gap_earnings_high_low = (school_data$MN_EARN_WNE_INC3_P10-school_data$MN_EARN_WNE_INC1_P10)/school_data$MN_EARN_WNE_INC3_P10
 school_data$gap_earnings_high_mid = (school_data$MN_EARN_WNE_INC3_P10-school_data$MN_EARN_WNE_INC2_P10)/school_data$MN_EARN_WNE_INC3_P10
 
 write.csv(school_data, file = "data/gap_metrics_data.csv")
+
+
+# Separate completion rates by race to remove rows with NA values
+cr_w_b <- school_data[, 1:6]
+cr_w_b <- na.omit(cr_w_b)
+cr_w_b$gap_completion_white_black = (cr_w_b$C150_4_WHITE-cr_w_b$C150_4_BLACK)/cr_w_b$C150_4_WHITE
+write.csv(cr_w_b, file="data/Completion_W_B.csv")
+
+cr_w_h <- school_data[, c(1:5,7)]
+cr_w_h <- na.omit(cr_w_h)
+cr_w_h$gap_completion_white_hispanic = (cr_w_h$C150_4_WHITE-cr_w_h$C150_4_HISP)/cr_w_h$C150_4_WHITE
+write.csv(cr_w_b, file="data/Completion_W_H.csv")
+
+cr_w_a <- school_data[, c(1:5, 8)]
+cr_w_a <- na.omit(cr_w_a)
+cr_w_a$gap_completion_white_asian = (cr_w_a$C150_4_WHITE-cr_w_a$C150_4_ASIAN)/cr_w_a$C150_4_WHITE
+write.csv(cr_w_b, file="data/Completion_W_A.csv")
+
+# Separate incomes rates out to remove rows with NA values
+income_rates <- school_data[, c(1:4, 17:19)]
+income_rates <-na.omit(income_rates)
+income_rates$gap_earnings_high_low = (income_rates$MN_EARN_WNE_INC3_P10-income_rates$MN_EARN_WNE_INC1_P10)/income_rates$MN_EARN_WNE_INC3_P10
+income_rates$gap_earnings_high_mid = (income_rates$MN_EARN_WNE_INC3_P10-income_rates$MN_EARN_WNE_INC2_P10)/income_rates$MN_EARN_WNE_INC3_P10
+income_rates$gap_earnings_mid_low = (income_rates$MN_EARN_WNE_INC2_P10-income_rates$MN_EARN_WNE_INC1_P10)/income_rates$MN_EARN_WNE_INC2_P10
+
+write.csv(income_rates, file="data/Income.csv")
